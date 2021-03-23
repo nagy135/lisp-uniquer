060b461b214f2ce13ead3c16377c8af8d90fe70d
Viktor Nagy <viktor.nagy1995@gmail.com>
2021-03-23 19:33:34 +0100

    feat: dummy file reading

---
+++ uniquer.lisp
@@ -0,0 +1,7 @@
+(let ((in (open "testfile" :if-does-not-exist nil)))
+  (when in
+    (loop for line = (read-line in nil)
+          while line do (format t "~a~%" line))
+    (close in)
+    )
+  )
